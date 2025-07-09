2872cdda9de3698189bfc88fe9a735864d6e1c94
Niveditha-J <nivedithaj005@gmail.com>
2025-07-09 11:52:45 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // for navbar toggler
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './components/Hero';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
